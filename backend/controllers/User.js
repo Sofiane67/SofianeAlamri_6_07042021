1db3fdf22b91d5538b8e1761ba07749fd0748da3
Sofiane <sa.dev672@gmail.com>
2021-04-07 22:58:46 +0200

    implémentation du login

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 exports.signup = (req, res, next) => {
     console.log(req.body)
@@ -15,4 +17,27 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(400).json({error}));
     })
     .catch(error => res.status(500).json({error}));
-}
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if(!user) return res.status(401).json({error: "Utilisateur non trouvé"});
+
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid) return res.status(401).json({error: "Mot de passe incorrect"});
+
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    {userId: user._id},
+                    process.env.TOKEN,
+                    {expiresIn: "24h"}
+                )
+            });
+        })
+        .catch(error => res.status(500).json({error}));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
