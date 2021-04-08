c3bd6b4358dbdd743772b8ae827ab2ced43f9528
Sofiane <sa.dev672@gmail.com>
2021-04-08 20:45:29 +0200

    Afficher toutes les sauces et une seul sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,5 +5,7 @@ const auth = require("../middleware/auth");
 const multer  = require("../middleware/multer-config");
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.get("/", auth, sauceCtrl.getAllSauces);
 
 module.exports = router;
