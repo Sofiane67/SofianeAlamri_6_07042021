adb9abdc3e4771e6d956c158190243e8bb402a3b
Sofiane <sa.dev672@gmail.com>
2021-04-08 21:02:50 +0200

    Modification d'une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -12,6 +12,16 @@ exports.createSauce = (req, res, next) => {
     .catch(error => res.status(400).json({error}));
 };
 
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+    } : {...req.body};
+    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+    .then(sauce => res.status(200).json({message: "Sauce modifiée"}))
+    .catch(error => res.status(400).json({error}));
+};
+
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
     .then(sauce => res.status(200).json(sauce))
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth");
 const multer  = require("../middleware/multer-config");
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
 
