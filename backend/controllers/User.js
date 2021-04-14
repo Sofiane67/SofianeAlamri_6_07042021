a29f01e449e669138c40f73eb3eee5d2e0b67fa9
Sofiane <sa.dev672@gmail.com>
2021-04-14 13:03:30 +0200

    Masquage de l'adresse email dans la bdd

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,14 +1,14 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cryptoJs = require("crypto-js");
 require('dotenv').config();
-
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
-            email: req.body.email,
+            email: cryptoJs.HmacSHA512(req.body.email, process.env.TOKEN).toString(cryptoJs.enc.Base64),
             password: hash
         });
 
@@ -20,7 +20,8 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
+    const email = cryptoJs.HmacSHA512(req.body.email, process.env.TOKEN).toString(cryptoJs.enc.Base64);
+    User.findOne({email: email})
     .then(user => {
         if(!user) return res.status(401).json({error: "Utilisateur non trouvé"});
 
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -21,9 +21,6 @@ exports.modifySauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
     } : {...req.body};
 
-
-
-    console.log(sauceObject);
     Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
     .then(() => res.status(200).json({message: "Sauce modifiée"}))
     .catch(error => res.status(400).json({error}));
